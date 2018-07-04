9ac9a1cd345bca80c4e0abcee1e4746d366b8abd
Chely Ho <adept_inept@yahoo.com>
2018-07-04 21:32:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,20 @@ class Customer {
     this.neighborhoodId = neighborhoodId
   }
 }
+
+class Meal {
+  constructor(title, price) {
+    this.id = mealId++
+    this.title = title
+    this.price = price
+  }
+}
+
+class Delivery {
+  constructor(mealId, neighborhoodId, customerId) {
+    this.id = deliveryId++
+    this.mealId = mealId
+    this.neighborhoodId = neighborhoodId
+    this.customerId = customerId
+  }
+}
